b46b9888b10572853cb5e15044256bc9d4b58ecf
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-04-19 14:48:07 -0700

    Add missing metatiles reference to game resources file to fix the "melonJS: "metatiles32x32" file for tileset "metatiles32x32" not found!" error message

--- tutorial_step3/js/resources.js
+++ tutorial_step3/js/resources.js
@@ -4,6 +4,8 @@ game.resources = [
 	 */
 	// our level tileset
 	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+	// our metatiles
+	{name: "metatiles32x32",  type:"image", src: "data/img/map/metatiles32x32.png"},
 	// the main player spritesheet
 	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
 	/* 
